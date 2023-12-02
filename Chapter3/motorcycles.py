c9db6df14c129482d576ad8368f11914e54c6ac8
Jacob Kruse <jacob.kruse@bananaclip.studio>
2023-12-02 04:24:14 -0500

    Completes instructions to page 41

---
+++ Chapter3/motorcycles.py
@@ -0,0 +1,19 @@
+motorcycles = ['honda', 'yamaha', 'suzuki']
+print(motorcycles)
+
+# motorcycles[0] = 'ducati'
+print(motorcycles)
+
+motorcycles.append('ducati')
+
+print(motorcycles)
+
+motorcycles.insert(1, 'harley')
+
+print(motorcycles)
+
+del motorcycles[0]
+
+print(motorcycles)
+
+
